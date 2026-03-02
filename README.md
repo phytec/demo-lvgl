956cb0616b089c969159c350ea2adfcc967654eb
Martin Schwan <m.schwan@phytec.de>
2026-03-02 11:37:20 +0100

    Port project to use LVGL v9.5.0
    The project is being ported to use LVGL v9.5.0 with all the necessary
    changes. Most notably, the xdg-shell is now used instead of wl-shell, as
    the latter is deprecated/no longer supported in LVGL. The required
    Wayland protocols are automatically generated by the build system
    (CMake). The LVGL configuration header is generated using a *.defaults
    file for easier maintenance.
    
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LVGL for Wayland
 
-LVGL configured to work with Wayland wl_shell on Linux.
+LVGL configured to work with Wayland `xdg_shell` on Linux.
 
 When cloning this repository, also make sure to download submodules (`git submodule update --init --recursive`) otherwise you will be missing key components.
 
---
+++ src/main.c
@@ -0,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 PHYTEC Messtechnik GmbH
+ */
+
+#include "lvgl.h"
+#include "demos/lv_demos.h"
+#include <unistd.h>
+
+int main(void)
+{
+    /* LVGL init */
+    lv_init();
+
+    /* Wayland display init */
+    lv_display_t *disp = lv_wayland_window_create(1280, 800, "window", NULL);
+    if (disp == NULL) {
+        LV_LOG_ERROR("Failed creating wayland window");
+        return 1;
+    }
+    lv_wayland_window_set_fullscreen(disp, true);
+
+    /* Set a cursor for the touchscreen */
+    lv_indev_t *touchscreen = lv_wayland_get_touchscreen(disp);
+    LV_IMG_DECLARE(mouse_cursor_icon)
+    lv_obj_t *cursor_obj = lv_img_create(lv_scr_act()); /* Create an image object for the cursor  */
+    lv_img_set_src(cursor_obj, &mouse_cursor_icon); /* Set the image source */
+    lv_indev_set_cursor(touchscreen, cursor_obj); /* Connect the image object to the driver */
+
+    /* Search connected keyboard and attach it to the driver */
+    lv_group_t *g = lv_group_create();
+    lv_group_set_default(g);
+    lv_indev_set_group(lv_wayland_get_keyboard(disp), g);
+    lv_indev_set_group(lv_wayland_get_pointeraxis(disp), g);
+
+    /* Create a Demo */
+    lv_demo_widgets();
+
+    uint32_t idle_time;
+    while (true) {
+        idle_time = lv_wayland_timer_handler();
+
+        if (idle_time != 0) {
+            usleep(idle_time * 1000);
+        }
+        if (!lv_wayland_window_is_open(NULL)) {
+            break;
+        }
+    }
+
+    return 0;
+}
